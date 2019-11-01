b917cc42d5ee9d4fb26e086ddb3962ebd0bf6b20
Marifatululfah17 <56728542+Marifatululfah17@users.noreply.github.com>
2019-11-01 04:36:48 -0700

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,11 @@
+#ini variabel & nilai
+
+namalengkap = "marifatul ulfah"
+namapanggilan ="ulfah"
+npm ="311910577"
+tempatlahir = "karawang"
+umur = "20"
+telf = "085780479386"
+alamat = "karawang"
+
+print("assalamualikum, \n let me introduce my self. My name is",namalengkap," but you can call me",namapanggilan,". i was born in",tempatlahir," and i am", umur,"years old. i am very glad if you want to invite my house in", alamat,". so, don't forget to call me before with the number", telf,"\n Thank you")
